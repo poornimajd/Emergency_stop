d0f7427c77d1ab58c79ecdd29bfa15a7bb15df48
poornimajd <31023599+poornimajd@users.noreply.github.com>
2021-01-22 13:47:27 +0530

    Update topic_activity_sub_better.py

--- topic_activity_sub_better.py
+++ topic_activity_sub_better.py
@@ -8,11 +8,11 @@ import threading
 pub=None
 
 def timeout():
-    
+    #if timeout, stop the vehicle by publishing the throttle,steer and brake values
     pub=rospy.Publisher('/cmds_for_brake',String,queue_size=5)
     rate=rospy.Rate(1)
     
-    while not rospy.is_shutdown():
+    while not rospy.is_shutdown():#to avoid confusion in case if netwrok restores during emergency stopping
     
         msg=String()
         msg.data="Throttle:0 Steer:0 Brake:1"
@@ -24,7 +24,7 @@ def callback_fun(msg):
 
     global timer
 
-    timer.cancel()
+    timer.cancel() #cancel the timer is the message is recieved from the /signal topic 
 
     timer = threading.Timer(0.2,timeout)
     timer.start()
@@ -33,7 +33,7 @@ def callback_fun(msg):
 if __name__=='__main__':
     rospy.init_node('signal_checker')
 
-    sub=rospy.Subscriber('/signal',String,callback_fun)
+    sub=rospy.Subscriber('/signal',String,callback_fun)#subscribe to the /signal topic on which the fake heartbeat signal is publishing
     print("heartbeat True")
     
     timer = threading.Timer(0.2,timeout) # If 0.2 seconds elapse, call timeout()
