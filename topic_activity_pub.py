1391108d9a049b885ef36686cdc6e7c3e396c709
poornimajd <31023599+poornimajd@users.noreply.github.com>
2021-01-22 13:43:59 +0530

    Update topic_activity_pub.py

--- topic_activity_pub.py
+++ topic_activity_pub.py
@@ -6,9 +6,9 @@ from std_msgs.msg import String
 
 
 if __name__=='__main__':
-    rospy.init_node("fake_beat_publisher")
+    rospy.init_node("fake_beat_publisher") #publish fake heartbeat signal on topic /signal
     pub=rospy.Publisher('/signal',String,queue_size=10)
-    rate=rospy.Rate(10)
+    rate=rospy.Rate(10) #at 10Hz
     while not rospy.is_shutdown():
         msg=String()
         msg.data='heartbeat recieved'
